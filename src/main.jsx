5b470d0cf80075b09c66f6c8e128482928b2b1ad
Daniel Cano Platero <ridedcp@gmail.com>
2025-10-24 11:56:13 +0200

    Initialize main.jsx with React and routing setup

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
+);
