b72fe8a843bf96cd1fc325506c2a4d8c6a019127
Daniel Cano Platero <ridedcp@gmail.com>
2025-10-31 20:53:55 +0100

    Change background color of Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 export default function Layout({ lang='es', children }) {
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
+    <div className="min-h-screen bg-gray-900">
       <Header lang={lang} />
       <main id="main-content">{children}</main>
       <Footer lang={lang} />
